6bbd70dba58cf9eb4655afbbb094dfbf0a4f98c3
Nathan <>
2019-08-27 22:50:35 +0800

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,47 @@
+<template>
+  <v-app>
+    <v-app-bar app>
+      <v-toolbar-title class="headline text-uppercase">
+        <span>Vuetify</span>
+        <span class="font-weight-light">MATERIAL DESIGN</span>
+      </v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-btn text href="https://github.com/vuetifyjs/vuetify/releases/latest" target="_blank">
+        <span class="mr-2">Latest Release</span>
+      </v-btn>
+    </v-app-bar>
+
+    <v-content>
+      <v-container fluid>
+        <v-row :align="start">
+          <FootballCard   v-for="match in matches" :key="match.matchId" :match="match"/>
+        </v-row>
+      </v-container>
+    </v-content>
+  </v-app>
+</template>
+
+<script>
+import FootballCard from "./components/FootballCard";
+
+export default {
+  name: "App",
+  components: {
+    FootballCard
+  },
+  data: () => ({
+    matches: [
+      {
+        matchId: 12,
+        home: "home1",
+        away: "away1"
+      },
+      {
+        matchId: 13,
+        home: "home2",
+        away: "away2"
+      }
+    ]
+  })
+};
+</script>
---
+++ src/components/FootballCard.vue
@@ -0,0 +1,28 @@
+<template>
+  <v-card >
+    <v-card-title>{{match.home}} 對 {{match.away}}</v-card-title>
+    <v-card-text>
+      <v-container>
+      <v-row> 主客和 </v-row>
+        <v-row>
+          <v-col xs6>
+            <h3>南鍚(主隊勝)</h3>
+            2.12
+          </v-col>
+          <v-col xs6>2</v-col>
+        </v-row>
+      </v-container>
+    </v-card-text>
+  </v-card>
+</template>
+
+<script>
+export default {
+  props: {
+    match: Object
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+</style>
